ebcc711e5d0ee999f291d27d772507719b205f4c
noisk8 <juansk8jaramillo@gmail.com>
2024-01-04 22:08:13 -0500

    grafico 1

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,12 +1,30 @@
+import { fetchRevenue } from '../lib/data';
+import RevenueChart from '../ui/dashboard/revenue-chart';
 import { lusitana, spacemono, turret } from '../ui/fonts';
 
-export default function page (){
+export default async function page (){
+
+    const revenue = await fetchRevenue()
+    console.log(revenue)
     return  <div>
 
-        <h1 className={`${turret.className}  text-center text-4xl font-bold text-green-300 ` }> Noisk8</h1>
+<main>
+<h1 className={`${turret.className}  text-center text-4xl font-bold text-green-300 ` }> Noisk8</h1>
+
+<div className='grid gap-6 sm:grid-cols-2 lg:grid-cols-4'>
+        {/* 
     <p className={`${turret.className}  text-center text-3xl  text-green-300 ` }> Woman in a box with her head in her box
 Speaks french when spoken like a tourist french
-Hey classic coca-cola </p>
+Hey classic coca-cola </p> */}
+</div>
+
+<div className='mt-6 grid grid-cols-1 gap-6 md:grid-cols4 lg:grid-cols-8'>
+    <RevenueChart revenue={revenue} />
+
+
+</div>
+</main>
+
     </div>
 }
 
